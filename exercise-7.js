ff91ac7191b193534b0df883df5392eed23c0c42
days88 <yayahidayatullah@rocketmail.com>
2018-08-23 22:05:41 +0800

    Digit Perkalian Minimum

--- exercise-7.js
+++ exercise-7.js
@@ -1,16 +1,33 @@
-function digitPerkalianMinimum(InputAngka) {
-	var output = 0
-	for (var i = 1; i < InputAngka + 1; i++) {
-		if(!output || output>(i+""+InputAngka/i).length){
-			output = (i+""+InputAngka/i).length;
-		}
-	}
-	return output;
-}
+function digitPerkalianMinimum(angka) {
+    var hasil =[];
+    // var nilai =[];
+    for(var i = 0;i <= angka;i++){
+        for(var j = 0;j <= angka;j++){
+            if(i * j === angka){
+                var hasilKali =''+i+j;
+                hasil.push(hasilKali);
+                // nilai.push(i);
+                // nilai.push(j);
+                // hasil.push(nilai);
+                // nilai = []
+            }
+        }
+    
+    }
+    var terkecil = hasil[0].length
+    for(var j = 0;j < hasil.length;j++){
+        if(hasil[j].length < terkecil){
+            terkecil = hasil[j].length
+        }
+    }
 
-// TEST CASES
-console.log(digitPerkalianMinimum(24)); // 2
-console.log(digitPerkalianMinimum(90)); // 3
-console.log(digitPerkalianMinimum(20)); // 2
-console.log(digitPerkalianMinimum(179)); // 4
-console.log(digitPerkalianMinimum(1)); // 2
+
+    return terkecil
+}    
+  
+  // TEST CASES
+  console.log(digitPerkalianMinimum(24)); // 2
+  console.log(digitPerkalianMinimum(90)); // 3
+  console.log(digitPerkalianMinimum(20)); // 2
+  console.log(digitPerkalianMinimum(179)); // 4
+  console.log(digitPerkalianMinimum(1)); // 2
