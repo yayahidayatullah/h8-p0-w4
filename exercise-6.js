faeaa354ac58aa21b69d0d167341798368704abb
days88 <yayahidayatullah@rocketmail.com>
2018-08-29 14:57:56 +0800

    Hitung Huruf

--- exercise-6.js
+++ exercise-6.js
@@ -1,16 +1,31 @@
 function hitungHuruf(kata) {
 
-    var kataSplit = kata.split(' ')
+    var kataSplit = kata.split(' ');
+    var hasil =[];
   
-    for(var i = 0; i < kataSplit.length; i++){
-      for(var j = 1; j < kataSplit[i].length - 1; j++){
-        for(var k = 1; k < kataSplit[i].length - 1; k++){
-          if(kataSplit[i][j] === kataSplit[i][j+k]){
-            return kataSplit[i]
-          } 
+    for(var i = 0; i < kataSplit.length;i++){
+      //console.log(kataSplit)
+      var jumlah = ['',[kataSplit[i]]];
+      for(var j = 0; j < kataSplit[i].length;j++){
+        //console.log('===',kataSplit[i][j])
+        for(var k = j+1;k < kataSplit[i].length;k++){
+          if(kataSplit[i][j] === kataSplit[i][k]&&kataSplit[i][j] !== jumlah[0][jumlah[0].length-1]){
+            jumlah[0]+=kataSplit[i][j];
+            
+          }
+          //console.log(kataSplit[i][k],'  ',jumlah[0])
         }
       }
+      hasil.push(jumlah)
+
     }
+    var terbesar = hasil[0];
+    for(var i=0;i<hasil.length;i++){
+        if (hasil[i][0].length > terbesar[0].length){
+            terbesar = hasil[i];
+        }
+    }
+    return terbesar[1][0]
   }
 
 // TEST CASES
